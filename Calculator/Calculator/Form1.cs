3d748ba826b3935e49e72466b6ce87f3efb5d5d8
Liu <frank23671199@gmail.com>
2025-06-06 21:16:32 +0800

    Debug in Calculator
    This code was a mess unnnnntil it meet my powerful debugger hehehe

---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+	public partial class Form1 : Form
+	{
+		int currentNum = 1;
+		double result, num1, num2;
+		String op;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		public void button_show()
+		{
+			addition.Enabled = true;
+			subtraction.Enabled = true;
+			multiplication.Enabled = true;
+			division.Enabled = true;
+		}
+
+        private void addition_Click(object sender, EventArgs e)
+        {
+            if (currentNum == 1)
+            {
+                num1 = Convert.ToDouble(textBox1.Text);
+                addition.Enabled = false;
+                op = "+";
+                textBox1.Text = "";
+                currentNum++;
+            }
+        }
+
+        private void Number0_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "0";
+        }
+
+        private void Number1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "1";
+        }
+
+        private void Number2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "2";
+        }
+
+        private void Number3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "3";
+        }
+
+        private void Number4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "4";
+        }
+
+        private void Number5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "5";
+        }
+
+        private void Number6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "6";
+        }
+
+        private void Number7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "7";
+        }
+
+        private void Number8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "8";
+        }
+
+        private void Number9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "9";
+        }
+     
+
+		private void button14_Click(object sender, EventArgs e)
+		{
+			textBox1.Text += "0";
+		}
+
+        private void subtraction_Click(object sender, EventArgs e)
+        {
+            if (currentNum == 1)
+            {
+                num1 = Convert.ToDouble(textBox1.Text);
+                subtraction.Enabled = false;
+                op = "-";
+                textBox1.Text = "";
+                currentNum++;
+            }
+        }
+
+        private void multiplication_Click(object sender, EventArgs e)
+        {
+            if (currentNum == 1)
+            {
+                num1 = Convert.ToDouble(textBox1.Text);
+                multiplication.Enabled = false;
+                op = "*";
+                textBox1.Text = "";
+                currentNum++;
+            }
+        }
+
+        private void division_Click(object sender, EventArgs e)
+        {
+            if (currentNum == 1)
+            {
+                num1 = Convert.ToDouble(textBox1.Text);
+                division.Enabled = false;
+                op = "/";
+                textBox1.Text = "";
+                currentNum++;
+            }
+        }
+
+        private void decimalPoint_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += ".";
+        }
+
+        private void equals_Click(object sender, EventArgs e)
+        {
+            num2 = Convert.ToDouble(textBox1.Text);
+            switch (op)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+            }
+            textBox1.Text = Convert.ToString(result);
+            currentNum++;
+            return;
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            button_show();
+            num1 = 0;
+            num2 = 0;
+            textBox1.Text = "";
+            currentNum = 1;
+        }
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # SEP_Calculator
- Fix the bug inside
+
+Fix the bug inside
+
+# The step of finding bugs
+
+1.  Design each testcase
+2.  Settup a workflow of Robottive
+3.  Tracing the code with Debugger
+
+# Bugs
+
+- some summing issue
+- input design problems
+- iligal operation (divided by zero)
+
+# How to run
+
+1.  Using Calculator.sln to cimpile the code
+2.  Run Calculator.exe
+
+# What have I learned
+
+- Testcase and Workflow Design
+- Robottive usage
+- Code Tracing skills (On shit code🐵🐵) p.s C# OMG
+- Jenkis auto testing (With skill of writing yaml)
